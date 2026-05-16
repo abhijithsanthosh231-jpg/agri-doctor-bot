fb4f093ee454778b56065c0d9c9a844b38b35689
abhijithsanthosh231-jpg <abhijithsanthosh231@gmail.com>
2026-05-16 18:04:02 +0530

    reverted to gemini 1.5

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post('/webhook', async (req, res) => {
   }
 
   try {
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-exp' });
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash', });
 
     let prompt;
     let imagePart = null;
